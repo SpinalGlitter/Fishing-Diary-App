6c191386dc7014edbe72b47d969ef07e8dc6830b
SpinalGlitter <johannes.persson47.gmail.com>
2025-08-03 23:01:19 +0200

    Starting the project, add som files

---
+++ FishingDiary/FishingDiary/FishingDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishingDiaryApp.swift
+//  FishingDiary
+//
+//  Created by Johannes Persson on 2025-08-03.
+//
+
+import SwiftUI
+
+@main
+struct FishingDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FishingDiary/FishingDiaryTests/FishingDiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  FishingDiaryTests.swift
+//  FishingDiaryTests
+//
+//  Created by Johannes Persson on 2025-08-03.
+//
+
+import Testing
+@testable import FishingDiary
+
+struct FishingDiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
